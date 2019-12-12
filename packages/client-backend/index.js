5d24a9acab7bdd1902d77d0f92ade43703830496
Sergio Bernal Guillot <sergioguillot@gmail.com>
2019-12-12 16:44:16 +0100

    feat(client-backend): healthcheck endpoint

--- packages/client-backend/index.js
+++ packages/client-backend/index.js
@@ -1,11 +1,17 @@
 var restify = require('restify');
 
 function respond(req, res, next) {
+  res.send('alive');
+  next();
+}
+
+function respond(req, res, next) {
   res.send('hello ' + req.params.name);
   next();
 }
 
 var server = restify.createServer();
+server.get('/healthcheck', respond);
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 
